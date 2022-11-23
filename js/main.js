20b7ccb35674f92309534a216663f7bc5788db7a
hello <sudanshakya15@gmail.com>
2022-11-23 21:15:29 +0545

    Created function for single round play

--- js/main.js
+++ js/main.js
@@ -9,3 +9,21 @@ function getPlayerChoice() {
     let playerInputLC = playerInput.toLowerCase();
     return playerInputLC;
 }
+
+function playRound(computerChoice, playerChoice) {
+    if(computerChoice ===  playerChoice){
+        return("It's a draw.");
+    } else if (
+        (computerChoice === "rock" && computerChoice === "paper") ||
+        (computerChoice === "paper" && playerChoice === "scissors") ||
+        (computerChoice === "scissors" && playerChoice === "rock")
+        ) {
+            return(`You win! ${(playerChoice).toUpperCase()} beats ${(computerChoice).toUpperCase()}.`);
+    } else if (
+        (computerChoice === "paper" && playerChoice === "rock") ||
+        (computerChoice === "scissors" && playerChoice === "paper") ||
+        (computerChoice === "rock" && playerChoice === "scissors")
+        ) {
+            return(`You lose! ${(computerChoice).toUpperCase()} beats ${(playerChoice).toUpperCase()}.`);
+    } 
+}
