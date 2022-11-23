e899e55e785c55471dac914f11a49f9473235011
hello <sudanshakya15@gmail.com>
2022-11-23 21:08:46 +0545

    Created function to get player's choice

--- js/main.js
+++ js/main.js
@@ -3,3 +3,9 @@ const choices = ["rock", "paper", "scissors"];
 function getComputerChoice() {
     return choices[Math.floor(Math.random() * choices.length)]; 
 }
+
+function getPlayerChoice() {
+    let playerInput = prompt("Enter your choice(Rock, Paper or Scissors): ");
+    let playerInputLC = playerInput.toLowerCase();
+    return playerInputLC;
+}
